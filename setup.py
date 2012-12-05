5c7ef1220fde555e6605049b3608f270ebadb959
Jeremy Stark <jlstark@gmail.com>
2012-12-05 09:46:47 -0500

    * Initial checkin of egg skel

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='jira',
+      version=version,
+      description="Jira command line tools",
+      long_description="""\
+Command line tools for Jira.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='jira command line',
+      author='Jeremy Stark',
+      author_email='jlstark@gmail.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
