041a138a4ca7537230fb26760edbe2a4dd38c83d
Jeremy Stark <jlstark@gmail.com>
2013-02-14 15:04:53 -0500

    * Add bug traversal option to links command

--- jira/commands/links/Plugin.py
+++ jira/commands/links/Plugin.py
@@ -7,9 +7,11 @@ class Command(BaseCommand):
     help = "Render an issue's link graphs"
     usage = 'links <issue_id> [-t <issue_type [...]>]'
     options_help = '''    -t : Show only issues of the specified type
+    -b : use bug traversal alogrithm (only descend development links)
     '''
     examples = '''    links 12345
     links NG-12345 -t 1 78
+    links NG-12345 -b
     '''
 
     def run(self, jira, args):
@@ -19,28 +21,39 @@ class Command(BaseCommand):
         parser = argparse.ArgumentParser()
         parser.add_argument('key')
         parser.add_argument('-t', nargs='*', required=False)
-        args = parser.parse_args(args)
-        story = jira.cache.get(args.key)
+        parser.add_argument('-b', action='store_true', required=False)
+        self.args = parser.parse_args(args)
+        story = jira.cache.get(self.args.key)
         if not story:
-            print 'Error: story key %s not found' % args.key
+            print 'Error: story key %s not found' % self.args.key
             return
         story = story[0]
+        if self.args.b:
+            self.args.t = ['1']
         print 'Release:'.ljust(15), 'Typ:', 'Stat:', ' Relationship Tree:'
-        print ''.join(story.fix_versions).ljust(15)[:15], str(story.type).ljust(4), str(story.status).ljust(6), story.key
+        print ''.join(story.fix_versions).ljust(15)[:15], \
+            str(story.type).ljust(4), str(story.status).ljust(6), story.key
         if story.links.outward:
             for link in story.links.outward:
-                if not args.t or link.type in args.t:
-                    print ''.join(link.fix_versions).ljust(15)[:15], str(link.type).ljust(4), str(link.status).ljust(6),'\-> %s' % link.key
-                self.recurse_links(link, [link.key], args.t)
+                if not self.args.t or link.type in self.args.t:
+                    print ''.join(link.fix_versions).ljust(15)[:15], \
+                        str(link.type).ljust(4), \
+                        str(link.status).ljust(6),'\-> %s' % link.key
+                if self.args.b and link.type not in self.args.t:
+                    continue
+                self.recurse_links(link, [link.key])
 
-    def recurse_links(self, issue, indent, types=[]):
+    def recurse_links(self, issue, indent):
         for link in issue.links.outward:
             if link.key in indent:
                 continue
-            if not types or link.type in types:
-                print ''.join(link.fix_versions).ljust(15)[:15], str(link.type).ljust(4), str(link.status).ljust(6), \
-                ''.ljust(len(indent[:-1])), '\-> %s' % link.key
+            if not self.args.t or link.type in self.args.t:
+                print ''.join(link.fix_versions).ljust(15)[:15], \
+                    str(link.type).ljust(4), str(link.status).ljust(6), \
+                    ''.ljust(len(indent[:-1])), '\-> %s' % link.key
+            if self.args.b and link.type not in self.args.t:
+                continue
             if link.links.outward:
                 indent.append(link.key)
-                self.recurse_links(link, indent, types)
+                self.recurse_links(link, indent)
                 del indent[0]
--- jira/features/links.feature
+++ jira/features/links.feature
@@ -34,3 +34,28 @@ Feature: Render issue link graph
         Then I see "-> NG-1" in the output
         And I see "-> NG-3" in the output
         And I do not see "-> MTQA-1" in the output
+
+    Scenario: A user changes the graph traversal method to tracking bugs
+        Given I have the following issues in the release
+        | key    | title | status | type | dev |
+        | NG-1   | Foo1  | 1      | 72   | joe |
+        | NG-2   | Bar2  | 2      | 72   | ann |
+        | NG-3   | Bar2  | 2      | 1    | ann |
+        | NG-4   | Baz3  | 3      | 1    | nic |
+        | NG-5   | Baz3  | 3      | 1    | nic |
+        And Issue "NG-1" has the following links
+        | key    |
+        | NG-2   |
+        | NG-3   |
+        And Issue "NG-3" has the following links
+        | key    |
+        | NG-5   |
+        And Issue "NG-2" has the following links
+        | key    |
+        | NG-4   |
+        When I enter the command "links NG-1 -b"
+        Then I see "NG-1" in the output
+        And I see "-> NG-3" in the output
+        And I see "-> NG-5" in the output
+        And I do not see "-> NG-2" in the output
+        And I do not see "-> NG-4" in the output
