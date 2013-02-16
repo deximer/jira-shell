e0c541d0ba092c2a6be2010acb4db712aa1787a1
Jeremy Stark <jlstark@gmail.com>
2013-02-16 14:58:37 -0500

    * Add interfaces module!

---
+++ jira/interfaces.py
@@ -0,0 +1,13 @@
+from zope.interface import Interface
+
+class IStory(Interface):
+    pass
+
+class IRelease(Interface):
+    pass
+
+class IProject(Interface):
+    pass
+
+class IKanban(Interface):
+    pass
