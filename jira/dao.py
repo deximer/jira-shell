c5477983981b82ca135799ad6d46f5e8d3bf8d08
Jeremy Stark <jlstark@gmail.com>
2013-03-22 14:03:54 -0400

    * Remove depricated dao.get_projects and related methods

--- jira/dao.py
+++ jira/dao.py
@@ -126,37 +126,6 @@ class Jira(object):
     def format_request(self, path):
         return 'http://%s@%s/%s' % (self.auth, self.server, path)
 
-    def request_page(self, path, refresh=True):
-        try:
-            if not refresh:
-                cache = open('cache.xml', 'r')
-                return cache.read()
-        except:
-            pass
-        page = urllib.urlopen(self.format_request(path)).read()
-        cache = open('cache.xml', 'w')
-        cache.write(page)
-        cache.close()
-        return page
-
-    def get_projects(self):
-        page = self.request_page('secure/BrowseProjects.jspa#all')
-        soup = BS(page)
-        names = soup.find('tbody', {'class' : 'projects-list'}).findAll(
-            'td')[::4]
-        codes = soup.find('tbody', {'class' : 'projects-list'}).findAll(
-            'td')[1::4]
-        owner = soup.find('tbody', {'class' : 'projects-list'}).findAll(
-            'td')[2::4]
-        projects = Projects()
-        for count in range(0, len(names)):
-            projects.add(Project(names[count].text, codes[count].text,
-                owner[count].text))
-        return projects
-
-    def all_projects(self):
-        return self.get_projects()
-
     def get_release(self, version=None):
         if not version:
             version = self.cwd[-1]
