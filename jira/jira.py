f148f7781b1e53fdaad2da8e9633bc5782b726c5
Jeremy Stark <jlstark@gmail.com>
2013-03-28 17:12:24 -0400

    * Playing with web server option

--- jira/jira.py
+++ jira/jira.py
@@ -159,7 +159,7 @@ def main():
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                self.wfile.write('Foo...')
+                self.wfile.write('/ > <input type="text" size="80" />')
                 return
         httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 9876), Handler)
         sa =httpd.socket.getsockname()
