687c60d6d2d26a109ed812d3dd45798b92e79cab
Stephen Vance <svance@zipcar.com>
2014-01-21 09:25:17 -0500

    Change the legend command to be dynamic

--- jira/commands/legend/Plugin.py
+++ jira/commands/legend/Plugin.py
@@ -1,17 +1,5 @@
 from ..base import BaseCommand
 
-STATUS_CODES = {
-    '1': 'Open',
-    '3': 'In Progress',
-    '4': 'Reopened',
-    '6': 'Closed',
-    '10036': 'Verified',
-    '10089': 'Ready',
-    '10090': 'Completed',
-    '10092': 'QA Active',
-    '10104': 'QA Ready'
-}
-
 ISSUE_TYPES = {
     '1' : 'Development Bug',
     '65': 'Configuration',
@@ -22,15 +10,36 @@ ISSUE_TYPES = {
     '156': 'Spike'
 }
 
+
 class Command(BaseCommand):
     help = 'List the meaning of the various codes'
     usage = 'legend'
+    STATUS_CODES = {}
+    ISSUE_TYPES = {}
 
     def run(self, jira, args):
+        self.fetch_status_codes(jira)
         print 'Status codes:'
-        for key in sorted(STATUS_CODES.keys()):
-            print key.ljust(6) + ':', STATUS_CODES[key]
+        for key in sorted(self.STATUS_CODES.keys(), key=lambda val: int(val)):
+            print key.ljust(6) + ':', self.STATUS_CODES[key]
         print
+        self.fetch_issue_types(jira)
         print 'Story types:'
-        for key in sorted(ISSUE_TYPES.keys()):
+        for key in sorted(ISSUE_TYPES.keys(), key=lambda val: int(val)):
             print key.ljust(6) + ':', ISSUE_TYPES[key]
+
+
+    def fetch_status_codes(self, jira):
+        if len(self.STATUS_CODES) != 0:
+            return
+        status_codes = jira.call_api('status')
+        for status in status_codes:
+            self.STATUS_CODES[status['id']] = status['name']
+
+
+    def fetch_issue_types(self, jira):
+        if len(self.ISSUE_TYPES) != 0:
+            return
+        issue_types = jira.call_api('issuetype')
+        for issue_type in issue_types:
+            self.ISSUE_TYPES[issue_type['id']] = issue_type['name']
