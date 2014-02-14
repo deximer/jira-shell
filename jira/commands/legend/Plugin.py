39168ab30f18dc2f072ab422b05c2025d2079029
Stephen Vance <svance@zipcar.com>
2014-02-14 10:06:03 -0500

    Fix the legend test for the new meta functionality and add the ability to get legends only for specific types.

--- jira/commands/legend/Plugin.py
+++ jira/commands/legend/Plugin.py
@@ -1,17 +1,28 @@
 from ..base import BaseCommand
 
+LEGEND_TYPES = {
+    'status': 'Status codes',
+    'issuetype': 'Issue types'
+}
+
 
 class Command(BaseCommand):
     help = 'List the meaning of the various codes'
-    usage = 'legend'
+    usage = 'legend [type]'
+    options_help = '''type is one of "status" or "issuetype"'''
+
+    def print_meta(self, jira, type):
+        codes = jira.fetch_meta(type)
+        print LEGEND_TYPES[type] + ':'
+        for key in sorted(codes.keys(), key=lambda val: int(val)):
+            print key.ljust(6) + ':', codes[key]
+        print
 
     def run(self, jira, args):
-        status_codes = jira.fetch_meta('status')
-        print 'Status codes:'
-        for key in sorted(status_codes.keys(), key=lambda val: int(val)):
-            print key.ljust(6) + ':', status_codes[key]
-        print
-        issue_types = jira.fetch_meta('issuetype')
-        print 'Story types:'
-        for key in sorted(issue_types.keys(), key=lambda val: int(val)):
-            print key.ljust(6) + ':', issue_types[key]
+        if len(args) == 0:
+            types = LEGEND_TYPES.keys()
+        else:
+            types = args
+
+        for type in types:
+            self.print_meta(jira, type)
